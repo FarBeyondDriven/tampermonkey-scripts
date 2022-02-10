76dee1252416541a68fee658dda9f8935d14ee18
Far Beyond Driven <3804214+FarBeyondDriven@users.noreply.github.com>
2022-02-10 23:45:13 +0000

    Create killBadEvents.user.js

---
+++ killBadEvents.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name KillBadEvents
+// @description Kill unwanted javascript events that hijack/block browser events
+// @version 59
+// @author FBD
+// @include http://*
+// @include https://*
+// @match http://*
+// @match https://*
+// @run-at document-start
+// @grant none
+// ==/UserScript==
+
+// add more events to block here
+var evts = ["contextmenu"];
+
+EventTarget.prototype.addEventListenerBase = EventTarget.prototype.addEventListener;
+
+EventTarget.prototype.addEventListener = function(type, listener) {
+  if (evts.includes(type)) {
+    console.log("Event trigger for " + type + " skipped");
+  } else {
+    console.log("Trigger activated for " + type);
+    this.addEventListenerBase(type, listener);
+  }
+};
